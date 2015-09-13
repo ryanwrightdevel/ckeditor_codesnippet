7ddb1f06eb35b40a88b3545db6705634de2bda62
Ryan Wright <ryanwrightdev@gmail.com>
2015-09-12 23:39:52 -0400

    added puppet to langauage

---
+++ js/config.js
@@ -0,0 +1,39 @@
+/**
+ * Created by brinx on 9/12/15.
+ */
+//the root config.js file in the ckeditor.
+
+CKEDITOR.editorConfig = function( config ) {
+  // Define changes to default configuration here. For example:
+  // config.language = 'fr';
+  // config.uiColor = '#AADC6E';
+  config.codeSnippet_languages = {
+    apache: 'Apache',
+    bash: 'Bash',
+    coffeescript: 'CoffeeScript',
+    cpp: 'C++',
+    cs: 'C#',
+    css: 'CSS',
+    diff: 'Diff',
+    html: 'HTML',
+    http: 'HTTP',
+    ini: 'INI',
+    java: 'Java',
+    javascript: 'JavaScript',
+    json: 'JSON',
+    makefile: 'Makefile',
+    markdown: 'Markdown',
+    nginx: 'Nginx',
+    objectivec: 'Objective-C',
+    perl: 'Perl',
+    php: 'PHP',
+    python: 'Python',
+    ruby: 'Ruby',
+    sql: 'SQL',
+    vbscript: 'VBScript',
+    xhtml: 'XHTML',
+    xml: 'XML',
+    vbnet: 'VB.NET',
+    puppet: 'Puppet'
+  };
+};
